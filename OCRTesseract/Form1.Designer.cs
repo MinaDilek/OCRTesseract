bb7114350084788214230b8dfdd274f839494cc3
mina dilek <minadilek7@outlook.com>
2021-03-17 22:47:35 +0300

    Proje dosyası ekle.

---
+++ OCRTesseract/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace OCRTesseract
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.txtHarf = new System.Windows.Forms.TextBox();
+            this.txtCumle = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "To Text";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(42, 99);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(314, 292);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.Location = new System.Drawing.Point(716, 218);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(117, 34);
+            this.btnGoster.TabIndex = 2;
+            this.btnGoster.Text = "Göster";
+            this.btnGoster.UseVisualStyleBackColor = true;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // txtHarf
+            // 
+            this.txtHarf.Location = new System.Drawing.Point(623, 153);
+            this.txtHarf.Name = "txtHarf";
+            this.txtHarf.Size = new System.Drawing.Size(262, 22);
+            this.txtHarf.TabIndex = 3;
+            // 
+            // txtCumle
+            // 
+            this.txtCumle.Location = new System.Drawing.Point(623, 97);
+            this.txtCumle.Name = "txtCumle";
+            this.txtCumle.Size = new System.Drawing.Size(262, 22);
+            this.txtCumle.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(450, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Cümle Giriniz";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(450, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "lHarf Giriniz";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(450, 374);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(48, 17);
+            this.lblSonuc.TabIndex = 7;
+            this.lblSonuc.Text = "Sonuç";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 450);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCumle);
+            this.Controls.Add(this.txtHarf);
+            this.Controls.Add(this.btnGoster);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnGoster;
+        private System.Windows.Forms.TextBox txtHarf;
+        private System.Windows.Forms.TextBox txtCumle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSonuc;
+    }
+}
+
---
+++ OCRTesseract/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract; 
+
+namespace OCRTesseract
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblSonuc.Text = " ";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                var img = new Bitmap(openFileDialog1.FileName);
+                var ocr = new TesseractEngine("./tessdata", "eng");
+                var sonuc = ocr.Process(img);
+                richTextBox1.Text = sonuc.GetText();
+            }
+        }
+
+        private void btnGoster_Click(object sender, EventArgs e)
+        {
+            string cumle = txtCumle.Text;
+
+            char Harf = Convert.ToChar(txtHarf.Text);
+            int harfSayisi = 0;
+
+            foreach (char i in cumle)
+            {
+                if (Harf == i)
+                    harfSayisi++;
+            }
+            lblSonuc.Text = Harf.ToString() + " Harfinin Toplam Sayısı:" + harfSayisi.ToString();
+
+
+
+
+
+
+        }
+    }
+}
